f14f1cf1374af14c79abc033442a896f7624242f
Oliver Strayer <strayeroliver@gmail.com>
2026-07-10 14:05:03 -0700

    new file for science

---
+++ all.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+import math
+
+def main():
+    for i in range(100):
+        print(i/10.0, 0, '   ', math.sin(i/10.0), 0)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+              
